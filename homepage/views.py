c001d7e510f9691085e009d396abb5ee7ce8afe9
Niko <niko@niko.io>
2015-10-11 12:20:01 +0200

    latest incidents on front page

--- homepage/views.py
+++ homepage/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from page.models import *
+from database.models import *
 
 def index(request):
     sections = Section.objects.all()
     blog_posts = Post.objects.all()
-    return render(request, 'homepage/index.html', {'sections' : sections, 'blog_posts':blog_posts,})
+    incidents = DatabaseEntry.objects.all()[:5]
+
+    return render(request, 'homepage/index.html', {'sections' : sections, 'blog_posts':blog_posts,'incidents':incidents,})
